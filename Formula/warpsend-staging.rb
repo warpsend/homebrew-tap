fd6adbce0e80023974e5a72f5423e517ad1c5d4f
Derek <ychiu1211@gmail.com>
2026-05-20 12:54:15 -0400

    Bump warpsend-staging.rb to build 92664c00645d

--- Formula/warpsend-staging.rb
+++ Formula/warpsend-staging.rb
@@ -7,7 +7,7 @@ class WarpsendStaging < Formula
   on_macos do
     on_arm do
       url "https://app-staging.warpsend.io/_agent/downloads/warpsend-aarch64-apple-darwin.tar.gz"
-      sha256 "0994125ab3bd9f67d1448273eeb76ae22ce25541c63df33cca7caca287cbd559"
+      sha256 "2d8670e63582c53d33ff816b755488f255a7a1dae22cd9d85b0361dcdd6b3af9"
     end
 
     on_intel do
