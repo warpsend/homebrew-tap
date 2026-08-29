8f441c02beb5f5b4fa985f124f7e929eed134268
Derek <ychiu1211@gmail.com>
2026-08-30 02:56:35 +0800

    Bump warpsend to build b7c0a37731c2

--- Casks/warpsend.rb
+++ Casks/warpsend.rb
@@ -1,8 +1,8 @@
 cask "warpsend" do
-  version "1.0.0.202608280245"
-  sha256 "5eec65ad62f6fa888af1f137bd02263ca71d74961286a007f4d157ea56ed5a27"
+  version "1.0.0.202608291554"
+  sha256 "1e9becc7787814d2baca499197f6f2b79fd35d4099ed72cab37ca2934447a1c5"
 
-  url "https://download.warpsend.io/desktop/393d7273a2fd/WarpSend-1.0.0-macos-aarch64.5eec65ad62f6.dmg"
+  url "https://download.warpsend.io/desktop/b7c0a37731c2/WarpSend-1.0.0-macos-aarch64.1e9becc77878.dmg"
   name "WarpSend"
   desc "NAS-to-NAS cross-region file transfer"
   homepage "https://warpsend.io"
